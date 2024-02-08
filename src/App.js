48bd1fa1b68a77371e333c1c2cfd562f8f993490
Soulbolt <soulbolt@gmail.com>
2024-02-07 17:39:50 -0700

    Add decrement and increment functions to the respective buttons.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import React from "react";
 class Counter extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+  // the use of this to access values is really needed everyone.. pretty much.
+  handleDecrement() {
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+
+  handleIncrement() {
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
   }
   render() {
     return (
       <div>
-        <button>-</button>
+        <button onClick={this.handleDecrement}>-</button>
         <span>{this.state.count}</span>
-        <button>+</button>
+        <button onClick={this.handleIncrement}>+</button>
       </div>
     );
   }
