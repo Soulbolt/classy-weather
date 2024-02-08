021bb8dccae9375a4a66d4016107d1b4febdf859
Soulbolt <soulbolt@gmail.com>
2024-02-07 17:30:15 -0700

    Add minus and plus buttons to a number counter in the middle.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 5 };
+  }
   render() {
     return (
       <div>
-        <button>
-          <span>0</span>
-        </button>
+        <button>-</button>
+        <span>{this.state.count}</span>
+        <button>+</button>
       </div>
     );
   }
